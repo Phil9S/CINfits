7b755344c5ed23eb93ea5c4a6701f02de88c12cd
Philip Smith <philsmith990@hotmail.co.uk>
2024-02-13 18:38:36 +0000

    Implement as refit
    Implemented code to refit allele-specific copy number. Uses allele ratio of each segment to total copy number to recompute allele-specific values at new ploidy/purity state.

--- R/rescaleFit.R
+++ R/rescaleFit.R
@@ -7,13 +7,12 @@
 #' @param old_purity old purity value from existing fit
 #' @param new_ploidy new ploidy value to refit copy number profile
 #' @param new_purity new purity value to refit copy number profile
-#' @param segValOnly return only segVal vector rather than entire refitted segment
-#'   table (Default: FALSE)
+#' @param alleleSpecific rescale allele-specific copy number data (default: FALSE)
 #'
 #' @return data.frame
 #' @export
 #'
-rescaleFit <- function(data=NULL,new_ploidy=NULL,new_purity=NULL,old_ploidy=NULL,old_purity=NULL,segValOnly=FALSE){
+rescaleFit <- function(data=NULL,new_ploidy=NULL,new_purity=NULL,old_ploidy=NULL,old_purity=NULL,alleleSpecific=FALSE){
 
     if(is.null(data)){
         stop("no data")
@@ -33,34 +32,46 @@ rescaleFit <- function(data=NULL,new_ploidy=NULL,new_purity=NULL,old_ploidy=NULL
 
     # cellploidy <- new_ploidy*new_purity + 2*(1-new_purity)
     # scaledploidy <- relploidy/cellploidy
-    #
-    scaled_cn_segVal <- convertCN(data,old_ploidy,old_purity,new_ploidy,new_purity)
+
+    if(alleleSpecific){
+        alleleRatioA <- data$nAraw / data$segVal
+        alleleRatioA[is.na(alleleRatioA)] <- 0.5
+        alleleRatioB <- 1-alleleRatioA
+    }
+
+    scaled_cn_segVal <- convertCN(data,
+                                  old_ploidy,
+                                  old_purity,
+                                  new_ploidy,
+                                  new_purity)
 
     data <- scaled_cn_segVal
-    if(segValOnly){
-        return(data$segVal)
-    } else {
-        return(data)
+
+    if(alleleSpecific){
+        data$nAraw <- data$segVal * alleleRatioA
+        data$nBraw <- data$segVal * alleleRatioB
     }
+
+    return(data)
 }
 
 ## depthtocn
 # support function to convert copy number to new ploidy purity combination
-convertCN<-function(v,old_ploidy,old_purity,new_ploidy,new_purity)
-{
-    old_sample_ploidy <- 2*(1-old_purity)+old_ploidy*old_purity
+convertCN<-function(v,old_ploidy,old_purity,new_ploidy,new_purity){
 
+    dip <- 2
     original_segVal <- v$segVal
-    test_rel <- (original_segVal*0.44)+(2*(1-old_purity))
+    old_sample_ploidy <- dip*(1-old_purity)+old_ploidy*old_purity
+    seg_rel <- (original_segVal*old_purity)+(dip*(1-old_purity))
 
     c <- v
-    c$segVal <- test_rel
+    c$segVal <- seg_rel
     new_rel_ploidy <- calculatePloidy(c)
 
-    new_sample_ploidy <- 2*(1-new_purity)+new_ploidy*new_purity
+    new_sample_ploidy <- dip*(1-new_purity)+new_ploidy*new_purity
     new_segdepth <- round(new_rel_ploidy/new_sample_ploidy,digits = 3)
 
-    c$segVal <- (c$segVal/new_segdepth-2*(1-new_purity))/new_purity
+    c$segVal <- (c$segVal/new_segdepth-dip*(1-new_purity))/new_purity
 
     #c$segVal[c$segVal < 0] <- 0
     #print(data.frame(v$segVal,c$segVal))
