744c4350c8f9d7245847903c2d2f5a45694f0161
Philip Smith <philsmith990@hotmail.co.uk>
2024-02-22 19:15:17 +0000

    update sunrise to allele-specific
    updated sunrise plot to use allele-specific implementation in refit function used internally.

--- R/plotSunrise.R
+++ R/plotSunrise.R
@@ -10,18 +10,24 @@
 #'   "seq.int(0.2,1,0.01)")
 #' @param ploidy ploidy value of current profile from which to calculate sunrise
 #' @param purity purity value of current profile from which to calculate sunrise
+#' @param alleleSpecific use allele-specific data
 #'
 #' @return plot
 #' @export
 #'
-plotSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,purity=NULL){
+plotSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,purity=NULL,alleleSpecifc=FALSE){
     if(is.null(ploidys)){
         ploidys <- seq.int(1.2,8,0.1)
     }
     if(is.null(purities)){
         purities <- seq.int(0.2,1,0.01)
     }
-    clonality <- calculateSunrise(data=data,ploidys = ploidys,purities = purities,ploidy=ploidy,purity=ploidy)
+    clonality <- calculateSunrise(data=data,
+                                  ploidys = ploidys,
+                                  purities = purities,
+                                  ploidy=ploidy,
+                                  purity=ploidy,
+                                  alleleSpecifc=alleleSpecifc)
 
     ## avoid R CMD check notes
     plpu <- data.frame(ploidy=c(as.character(ploidy)),
@@ -35,11 +41,13 @@ plotSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,purity=
         #geom_point(data=plpu,shape=4, size = 10) +
         ggplot2::scale_x_discrete(breaks = unique(round(ploidys,digits = 0))) +
         ggplot2::scale_y_discrete(breaks = unique(round(purities,digits = 1))) +
+        ggplot2::scale_fill_gradient(low = "blue",high = "yellow") +
+        #ggplot2::scale_fill_gradient2(low = "blue",mid = "white",high = "yellow",midpoint = midpoint) +
         ggplot2::theme_bw() +
         ggplot2::theme(legend.position = "bottom")
 }
 
-calculateSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,purity=NULL){
+calculateSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,purity=NULL,alleleSpecifc=FALSE){
     if(is.null(ploidys)){
         ploidys <- seq.int(1.2,8,0.1)
     }
@@ -50,7 +58,8 @@ calculateSunrise <- function(data=NULL,ploidys=NULL,purities=NULL,ploidy=NULL,pu
         calculateClonality(data = rescaleFit(data,
                                              old_ploidy = ploidy,
                                              old_purity = purity,
-                                             new_ploidy = x,new_purity = y))
+                                             new_ploidy = x,new_purity = y,
+                                             alleleSpecific = alleleSpecifc))
     }
 
     clonality <- sapply(ploidys, function(x) mapply(FUN = comp_clonality,x,purities))
