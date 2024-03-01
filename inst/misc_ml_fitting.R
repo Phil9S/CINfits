d5c5eac45117179695ab668fbc0612871867d0ef
Philip Smith <philsmith990@hotmail.co.uk>
2024-03-01 19:49:06 +0000

    misc ml code
    misc temp ml code

--- inst/misc_ml_fitting.R
+++ inst/misc_ml_fitting.R
@@ -11,7 +11,8 @@ depthtocn<-function(x,purity,seqdepth) #converts readdepth to copy number given 
     (x/seqdepth-2*(1-purity))/purity
 }
 
-meta.old <- read.table("C:/Users/Phil/Downloads/OneDrive_1_26-01-2024/fit_QC_predownsample_withsmooth.tsv",header = T,sep = "\t")
+#meta.old <- read.table("C:/Users/Phil/Downloads/OneDrive_1_26-01-2024/fit_QC_predownsample_withsmooth.tsv",header = T,sep = "\t")
+meta.old <- read.table("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/fit_QC_predownsample_withsmooth.tsv",header = T,sep = "\t")
 
 qc.data <- meta.old %>%
             select(SAMPLE_ID,ploidy,purity,use) %>%
@@ -20,7 +21,7 @@ qc.data <- meta.old %>%
             group_by(SAMPLE_ID) %>%
             filter(const == TRUE)
 
-rds.rel <- readRDS("C:/Users/Phil/Downloads/OneDrive_1_26-01-2024/britroc_smoothed_copyNumbersSegmented.rds")
+rds.rel <- readRDS("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/britroc_smoothed_copyNumbersSegmented.rds")
 
 samples <- qc.data[which(qc.data$SAMPLE_ID %in% colnames(rds.rel)),]
 rds.rel <- rds.rel[,which(colnames(rds.rel) %in% qc.data$SAMPLE_ID)]
@@ -69,12 +70,16 @@ m.data <- stats %>%
     left_join(.,qc.data,by = "SAMPLE_ID") %>%
     select(-c(ploidy.y,const)) %>%
     rename("ploidy" = "ploidy.x") %>%
-    tibble::column_to_rownames("SAMPLE_ID")
+    tibble::column_to_rownames("SAMPLE_ID") %>%
+    mutate(use = as.factor(use))
+
+m.data.nocn <- m.data %>%
+                select(-contains("cnstate"))
 
 data <- m.data
-data$use <- as.factor(data$use)
+#data <- m.data.nocn
 
-set.seed(0990)
+set.seed(134)
 ind <- sample(2, nrow(data), replace = TRUE, prob = c(0.7, 0.3))
 train <- data[ind==1,]
 test <- data[ind==2,]
@@ -85,7 +90,7 @@ print(rf)
 p1 <- predict(rf, train)
 confusionMatrix(p1, train$use)
 
-p2 <- predict(rf, test)
+p2 <- predict(rf, test,type = "class")
 confusionMatrix(p2, test$use)
 
 plot(rf)
