c0a68e4cf4e0914bd09337a0f1647ea7a00df3f0
Philip Smith <philsmith990@hotmail.co.uk>
2025-08-13 15:45:49 +0100

    Refactor and extend absolute CN analysis script
    Reorganizes and cleans up the script for absolute copy number analysis, moving function definitions and data loading to the top, and removing unused or redundant code. Adds a new function to calculate segment variance, updates the fit statistics calculation to use absolute copy number, and outputs a summary table for downstream analysis. Also includes improved variable naming and data handling for clarity and reproducibility.

--- inst/new_feature_testing.R
+++ inst/new_feature_testing.R
@@ -1,54 +1,10 @@
-readRDS()
-
-
-rm(list = ls())
-args = commandArgs(trailingOnly=TRUE)
-
-#rds.list <- list.files(path = ".",pattern = "*.rds")
-bin <- "30"
-project <- "britroc"
-
-qc.data <- read.table("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/fit_QC_predownsample.tsv",header = T,sep = "\t")
-#qc.data <- qc.data[qc.data$use == "TRUE",]
-#refit.params <- read.table("refitting_parameters_updated.csv",header = T,sep = ",")
-output_dir <- file.path("absolute_POST_down_sampling/")
-rdsdata <- paste0("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/britroc_smoothed_copyNumbersSegmented.rds")
-
-if(!dir.exists("absolute_POST_down_sampling/plots")){
-    dir.create("absolute_POST_down_sampling/plots")
-}
-
 #load libraries
 library(QDNAseqmod)
 library(Biobase)
 library(ggplot2)
 library(stringr)
 
-# convert depth to abs cn
-depthtocn<-function(x,purity,seqdepth) #converts readdepth to copy number given purity and single copy depth
-{
-    (x/seqdepth-2*(1-purity))/purity
-}
 
-# Combine and load rds objects
-rds.rel <- readRDS(rdsdata)
-# List samples
-samples <- qc.data[which(qc.data$SAMPLE_ID %in% colnames(rds.rel)),]
-
-# List smoothed samples
-#smoothed_samples <- refit.params$name[refit.params$smooth == "TRUE"]
-
-# Add pheno information
-pData(rds.rel)$purity <- samples$purity[match(pData(rds.rel)$name,samples$SAMPLE_ID)]
-pData(rds.rel)$ploidy <- samples$ploidy[match(pData(rds.rel)$name,samples$SAMPLE_ID)]
-pData(rds.rel)$TP53freq <- samples$TP53freq[match(pData(rds.rel)$name,samples$SAMPLE_ID)]
-pData(rds.rel)$PATIENT_ID <- samples$PATIENT_ID[match(pData(rds.rel)$name,samples$SAMPLE_ID)]
-
-# Generate abs plot and table of fits
-res <- data.frame(matrix(ncol = 9, nrow = 0))
-abs_profiles <- rds.rel[fData(rds.rel)$use,]
-
-# # For each
 #getsegtable
 getSegTable <- function(x){
     if(inherits(x,what = "QDNAseqCopyNumbers",which = F)){
@@ -93,14 +49,52 @@ getSegTable <- function(x){
     }
 }
 
-rds.pdata <- pData(abs_profiles)
-rds.obj <- abs_profiles[,colnames(abs_profiles) %in% samples$SAMPLE_ID]
+# convert depth to abs cn
+depthtocn <- function(x,purity,seqdepth) #converts read depth to copy number given purity and single copy depth
+{
+    (x/seqdepth-2*(1-purity))/purity
+}
 
+calculateSegmentVar <- function(x,y){
+    segRLE <- rle(x)
+
+    segVar <- c()
+    for(i in 1:length(segRLE$lengths)){
+        if(i == 1){
+            strt_idx <- 1
+            end_idx <- segRLE$lengths[i]
+            segVar <- append(segVar,var(y[strt_idx:end_idx]))
+        } else {
+            start_idx <- max(cumsum(segRLE$lengths[1:i-1]))
+            strt_idx <- start_idx + 1
+            if(i == length(segRLE$lengths)){
+                end_idx <- segRLE$lengths[i] + strt_idx - 1
+            } else {
+                end_idx <- segRLE$lengths[i] + strt_idx
+            }
+            segVar <- append(segVar,var(y[strt_idx:end_idx]))
+        }
+    }
+
+    medianVar <- median(segVar)
+    return(medianVar)
+}
+
+qc.data <- read.table("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/fit_QC_predownsample.tsv",header = T,sep = "\t")
+rdsdata <- paste0("../../OneDrive - CRUK Cambridge Institute/britroc-cn-analysis/absolute_PRE_down_sampling/britroc_smoothed_copyNumbersSegmented.rds")
+
+rds.rel <- readRDS(rdsdata)
+samples <- qc.data[which(qc.data$SAMPLE_ID %in% colnames(rds.rel)),]
+
+abs_profiles <- rds.rel[fData(rds.rel)$use,]
+
+#rds.pdata <- pData(abs_profiles)
+rds.obj <- abs_profiles[,colnames(abs_profiles) %in% samples$SAMPLE_ID]
 
 fitstats <- do.call(rbind,lapply(colnames(rds.obj),FUN = function(sample){
 
-    ploidies<-samples$ploidy[samples$SAMPLE_ID == sample]
-    purities<-samples$purity[samples$SAMPLE_ID == sample]
+    ploidies <- samples$ploidy[samples$SAMPLE_ID == sample]
+    purities <- samples$purity[samples$SAMPLE_ID == sample]
 
     ind<-which(colnames(rds.obj)==sample)
     relcn<-rds.obj[,ind]
@@ -110,59 +104,30 @@ fitstats <- do.call(rbind,lapply(colnames(rds.obj),FUN = function(sample){
     # added by PS
     copynumber<-assayDataElement(relcn,"copynumber")
     rel_ploidy<-mean(copynumber,na.rm=T)
-    print(sample)
+
+    cn<-assayDataElement(relcn,"copynumber")
+    seg<-assayDataElement(relcn,"segmented")
+    cn<-as.numeric(cn[!is.na(cn),])
+    seg<-as.numeric(seg[!is.na(seg),])
 
     res <- do.call(rbind,lapply(1:length(ploidies),FUN = function(i){
         ploidy <- ploidies[i]
         purity <- purities[i]
+
         cellploidy<-ploidy*purity+2*(1-purity)
         seqdepth<-rel_ploidy/cellploidy
 
-        cn<-assayDataElement(relcn,"copynumber")
-        seg<-assayDataElement(relcn,"segmented")
-        cn<-as.numeric(cn[!is.na(cn),])
-        seg<-as.numeric(seg[!is.na(seg),])
-        integer_cn<-round(depthtocn(seg,purity,seqdepth))
-
-
-        abs_cn<-depthtocn(seg,purity,seqdepth)
-
-
+        abs_seg <- depthtocn(seg,purity,seqdepth)
         abs_cnbin <- depthtocn(cn,purity,seqdepth)
 
-        calculateSegmentVar <- function(x,y){
-            segRLE <- segRLE <- rle(x)
-
-            segVar <- c()
-            for(i in 1:length(segRLE$lengths)){
-                if(i == 1){
-                    strt_idx <- 1
-                    end_idx <- segRLE$lengths[i]
-                    segVar <- append(segVar,var(y[strt_idx:end_idx]))
-                } else {
-                    start_idx <- max(cumsum(segRLE$lengths[1:i-1]))
-                    strt_idx <- start_idx + 1
-                    if(i == length(segRLE$lengths)){
-                        end_idx <- segRLE$lengths[i] + strt_idx - 1
-                    } else {
-                        end_idx <- segRLE$lengths[i] + strt_idx
-                    }
-                    segVar <- append(segVar,var(y[strt_idx:end_idx]))
-                }
-            }
-
-            medianVar <- median(segVar)
-            return(medianVar)
-        }
-
-        calculateSegmentVar(seg,cn)
+        segmentVar <- calculateSegmentVar(abs_seg,abs_cnbin)
 
         relcn_temp <- relcn
-        assayDataElement(relcn_temp,"segmented")[,1] <- abs_cn
+        assayDataElement(relcn_temp,"segmented")[,1] <- abs_seg
 
         tab <- getSegTable(relcn_temp)
 
-        stats <- as.data.frame(cbind(calculateCINStats(tab),seg_var=calculateSegmentVar(seg,cn)))
+        stats <- as.data.frame(cbind(calculateCINStats(tab),segvariance=segmentVar))
 
         stats <- stats %>% rownames_to_column("sample") %>%
             mutate(grid_ploidy = round(rep(ploidy,times=nrow(.)),digits = 1)) %>%
@@ -176,4 +141,14 @@ fitstats_ord <- fitstats[order(fitstats$sample,fitstats$clonality),]
 all(fitstats_ord$sample == qc.data$SAMPLE_ID)
 fitstats_ord$use <- qc.data$use
 
+cellQC <- fitstats_ord %>% ## generated in
+    tidyr::drop_na() %>%
+    dplyr::select(-grid_ploidy) %>%
+    dplyr::rename(purity = grid_purity) %>%
+    dplyr::mutate(use = factor(use))
+write.table(cellQC,file = "inst/britroc_multifit_meta.tsv",append = F,quote = F,sep = "\t",row.names = F,col.names = T)
 
+fitstats_ord_single <- fitstats_ord %>% ## remove sample duplicates and fit single profile
+                        group_by(sample) %>%
+                        filter(row_number() == 1 | use == TRUE) %>%
+                        filter(row_number() == 1)
